d6066d92900b931d3e2680d2c8a388d4a53601c8
khazadoom <31121986g@gmail.com>
2019-09-29 08:28:27 +0530

    Learning Memoize

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-const font = "";
+// type Style = number | string | undefined;
+// const font: Style = ""
+const memoizee = require('memoizee');
+// import { memoizee } from 'memoizee';
+function add(a, b) {
+    console.log(`add is called on ${a} and ${b}`);
+    return a + b;
+}
+const memoizedAdd = memoizee(add);
+memoizedAdd(1, 2); // log "add is called"
+memoizedAdd(1, 2); // cache hit, not logging
+memoizedAdd(1, 3); // log "add is called"
--- index.ts
+++ index.ts
@@ -1,3 +1,19 @@
-type Style = number | string | undefined;
+// type Style = number | string | undefined;
 
-const font: Style = ""
+// const font: Style = ""
+
+const memoizee = require('memoizee');
+
+// import { memoizee } from 'memoizee';
+
+
+function add(a: number, b: number): number {
+  console.log('add is called');
+  return a + b;
+}
+
+const memoizedAdd = memoizee(add);
+
+memoizedAdd(1,2);  // log "add is called"
+memoizedAdd(1,2);  // cache hit, not logging
+memoizedAdd(1,3);  // log "add is called"
